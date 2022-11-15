e0e9edd03ac51087754a95a675161cc8628c3517
Roman <roman.lemontagner@gmail.com>
2022-11-15 14:48:52 +0100

    add multiwavelength lightcurves

--- notebooks/script/plot.py
+++ notebooks/script/plot.py
@@ -1,7 +1,13 @@
 import numpy as np
+import pandas as pd
 
 from collections import Counter
 import healpy as hp
+
+import matplotlib.pyplot as plt
+
+from astropy.time import Time
+import swifttools.ukssdc.data.SXPS as uds
 
 def plot_mwd(fig, RA,Dec,color,ax, cmap='viridis', alpha=0.5, cb=True,org=0,title=None, cb_title='magnitude',projection='mollweide'):
     ''' RA, Dec are arrays of the same length.
@@ -64,3 +70,112 @@ def get_ppix(pdf, nside=1024):
     ppixs = [pdict[i] for i in pixs]
     
     return ppixs
+
+
+def plot_xlc(ax, data, band, band_label):
+
+    if band in data:
+        ytop = data[band]["RatePos"]
+        ybot = data[band]["RateNeg"]
+
+        xright = data[band]["TimePos"]
+        xleft = data[band]["TimeNeg"]
+
+        ax.errorbar(
+            data[band]["Time"],
+            data[band]["Rate"],
+            yerr=[-ybot, ytop],
+            xerr=[-xleft, xright],
+            label=band_label,
+            capsize=2, elinewidth=1, marker='.', linestyle='none'
+        )
+
+        return data[band]["Time"], data[band]["Rate"]
+    else:
+        return pd.Series([], dtype=np.float64), pd.Series([], dtype=np.float64)
+
+
+start_fink = Time("2019-11-01")
+def plot_mw_lc(data, objectId):
+    current_obj = data[data["objectId"] == objectId].sort_values("jd")
+    lsxps_id = current_obj["LSXPS_ID"].values[0]
+
+    lcs = uds.getLightCurves(
+        sourceID=int(lsxps_id),
+        cat='LSXPS',
+        binning='obsid',
+        timeFormat='MJD',
+        saveData=False,
+        returnData=True
+    )
+
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(18, 10))
+
+    fig.suptitle('{} - {} - {}'.format(current_obj['objectId'].values[0],
+              current_obj['fink_class'].values[0], current_obj['IAUName'].values[0]))
+
+    dicname = {1: 'g', 2: 'r'}
+    for filt in [1, 2]:
+        mask = current_obj['fid'].values == filt
+
+        if np.sum(mask) == 0:
+            continue
+        ax1.errorbar(
+            current_obj['jd'].values[mask],
+            current_obj['dcmag'].values[mask],
+            current_obj['dcmag_err'].values[mask],
+            ls='',
+            marker='.',
+            label=dicname[filt],
+            color='C{}'.format(filt - 1)
+        )
+
+    ax1.set_ylabel('DC magnitude')
+    ax1.set_xlabel('Days since the beginning of Fink')
+    ax1.invert_yaxis()
+
+    plt.sca(ax1)
+    plt.vlines(
+        start_fink.jd,
+        np.min(current_obj['dcmag'].values),
+        np.max(current_obj['dcmag'].values),
+        colors="red",
+        linestyles='dotted',
+        label="FINK start date"
+    )
+    plt.xticks(
+        current_obj['jd'].values[::45], 
+        (current_obj['jd'].values - start_fink.jd)[::45].astype(int),
+        rotation=90
+    )
+    ax1.legend()
+
+    soft_time, soft_rate = plot_xlc(ax2, lcs, "Soft_rates", "0.3 - 1 keV")
+    medium_time, medium_rate = plot_xlc(ax2, lcs, "Medium_rates", "1 - 2 keV")
+    hard_time, hard_rate = plot_xlc(ax2, lcs, "Hard_rates", "2 - 10 keV")
+
+    xray_time = pd.concat([soft_time, medium_time, hard_time]).sort_values()
+    xray_rate = pd.concat([soft_rate, medium_rate, hard_rate])
+    jd_xray = Time(xray_time, format="mjd").jd
+
+    plt.sca(ax2)
+    plt.vlines(
+        start_fink.mjd, 
+        np.min(xray_rate),
+        np.max(xray_rate), 
+        colors="red",
+        linestyles='dotted',
+        label="FINK start date"
+    )
+    plt.xticks(
+        xray_time, 
+        (jd_xray - start_fink.jd).astype(int),
+        rotation=90
+    )
+
+    ax2.legend()
+    ax2.set_ylabel('Rate')
+    ax2.set_xlabel('Days since the beginning of Fink')
+
+    plt.tight_layout()
+    plt.show()
